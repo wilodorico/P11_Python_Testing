42039479ef8443b0981b9cc313f54b8fdb5de675
wilfried <wilfried.odorico@gmail.com>
2025-05-16 19:05:16 +0200

    add test for non existent file return exception FileNotFoundError with message

--- tests/json_services/test_json_services.py
+++ tests/json_services/test_json_services.py
@@ -1,5 +1,7 @@
 import json
 
+import pytest
+
 from json_services import JSONServices
 
 
@@ -9,3 +11,10 @@ def test_load_valid_json_file(mocker):
     mocker.patch("builtins.open", mocker.mock_open(read_data=json.dumps(mock_data)))
     result = JSONServices.load("clubs.json")
     assert result == mock_data
+
+
+def test_load_non_existent_json_file(mocker):
+    mocker.patch("builtins.open", side_effect=FileNotFoundError)
+    with pytest.raises(FileNotFoundError) as exc_info:
+        JSONServices.load("non_existent_file.json")
+    assert str(exc_info.value) == "File non_existent_file.json not found."
