4d29e2df099ddbf09f71b6de96b506e49d06696c
wilfried <wilfried.odorico@gmail.com>
2025-05-16 16:09:54 +0200

    Add test with non existent email in base and rename test for clarity

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,10 @@
-def test_login_with_non_existing_email(client):
+def test_login_without_email(client):
     response = client.post("/showSummary", data={"email": ""})
     assert response.status_code == 200
     assert b"Please enter an email" in response.data
+
+
+def test_login_with_non_existent_email(client):
+    response = client.post("/showSummary", data={"email": "nonexistent@test.com"})
+    assert response.status_code == 200
+    assert b"Email not found" in response.data
