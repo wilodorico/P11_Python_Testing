272325467f80a9ccd900461b5964987f1e74959a
wilfried <wilfried.odorico@gmail.com>
2025-05-16 19:05:37 +0200

    Add error handling for FileNotFoundError in JSONServices.load method

--- json_services.py
+++ json_services.py
@@ -4,5 +4,8 @@ import json
 class JSONServices:
     @staticmethod
     def load(filename: str):
-        with open(filename) as file:
-            return json.load(file)
+        try:
+            with open(filename) as file:
+                return json.load(file)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File {filename} not found.")
