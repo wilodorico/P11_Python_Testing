45eb5bbb8319bd29f24cbc607b61f06cc591c3bc
wilfried <wilfried.odorico@gmail.com>
2025-05-16 13:00:25 +0200

    Add test configuration and fixtures; update .gitignore to include .flake8 and retrieve tests/

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from server import app as flask_app
+
+
+@pytest.fixture
+def app():
+    flask_app.config.update({"TESTING": True})
+    yield flask_app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
