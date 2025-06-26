37354d088b4a078c76afc15bb68476ced3188fc7
wilfried <wilfried.odorico@gmail.com>
2025-06-26 10:43:57 +0200

    Add methods to get club reservations and total reserved places for competitions

--- repositories/reservation_json_repository.py
+++ repositories/reservation_json_repository.py
@@ -23,6 +23,17 @@ class ReservationJsonRepository(ReservationRepository):
     def all(self) -> list[Reservation]:
         return self._reservations
 
+    def get_club_reservations_for_competition(self, club_id: str, competition_id: str) -> list[Reservation]:
+        return [
+            reservation
+            for reservation in self._reservations
+            if reservation.club_id == club_id and reservation.competition_id == competition_id
+        ]
+
+    def get_total_places_club_reservation_for_competition(self, club_id: str, competition_id: str) -> int:
+        reservations = self.get_club_reservations_for_competition(club_id, competition_id)
+        return sum(reservation.reserved_places for reservation in reservations)
+
     def save(self, reservation: Reservation) -> None:
         self._add(reservation)
         serialized_data = [reservation.serialize() for reservation in self._reservations]
